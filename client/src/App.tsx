df6a1bf583e60784314af795e3dd4cfed45d4f51
kmlee95 <kmlee95@naver.com>
2021-06-09 23:07:57 +0900

    Feat #15 : 로그인페이지 라우팅 설정 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -30,9 +30,8 @@ function App(): JSX.Element {
             <Route path="/project" exact component={Project} />
             <Route path="/calendar" exact component={Calendar} />
             <Route path="/question" exact component={Question} />
-
-            <Route path="/signin" exact component={Signin} />
             <Route path="/signup" exact component={Signup} />
+            <Route path="/signin" exact component={Signin} />
             <Route path="/testpage" exact component={TestPage} />
           </AppLayout>
         </Switch>
---
+++ client/src/pages/Signin/styled.ts
@@ -0,0 +1,5 @@
+import styled from '@emotion/styled';
+
+import theme from '@/theme';
+
+export const SignIn = styled.div``;
