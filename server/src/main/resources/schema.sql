b1327853209cc8e04657f910e23d16090a9c3a30
Hyojin Song <hyojin1076@naver.com>
2021-07-03 01:00:32 +0900

    test: 정렬

--- server/src/main/resources/schema.sql
+++ server/src/main/resources/schema.sql
@@ -2,17 +2,17 @@ DROP TABLE IF EXISTS account CASCADE;
 
 CREATE TABLE account
 (
-    id           bigint      NOT NULL AUTO_INCREMENT,
-    name          varchar(10) NOT NULL,
-    email         varchar(50) NOT NULL,
-    password        varchar(80) NOT NULL,
-    nickname        varchar(100) NOT NULL,
-    part        varchar(100) NOT NULL,
-    role        varchar(100) NULL,
-    alarm_flag        varchar(100) NULL,
-    introduce        varchar(100) NULL,
-    profilePath        varchar(100) NULL,
-    create_date     datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP(), -- 생성일시
-    modified_date     datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP(), -- 생성일시
+    id                  bigint          NOT NULL AUTO_INCREMENT,
+    name                varchar(10)     NOT NULL,
+    email               varchar(50)     NOT NULL,
+    password            varchar(80)     NOT NULL,
+    nickname            varchar(100)    NOT NULL,
+    part                varchar(100)    NOT NULL,
+    role                varchar(100)    NOT NULL,
+    alarm_flag          varchar(100)    NOT NULL,
+    introduce           varchar(100)    NULL,
+    profilePath         varchar(100)    NULL,
+    create_date         datetime        NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    modified_date       datetime        NOT NULL DEFAULT CURRENT_TIMESTAMP(),
     PRIMARY KEY (id)
 );
