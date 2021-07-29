2503384140a510b0391609fdb39add7c8ca19295
Hyojin Song <hyojin1076@naver.com>
2021-07-30 01:31:58 +0900

    test: 시큐리티 구현부 수정으로 인한 테스트 코드 변경

--- server/src/test/java/com/doongji/homepage/security/JwtTest.java
+++ server/src/test/java/com/doongji/homepage/security/JwtTest.java
@@ -2,6 +2,12 @@ package com.doongji.homepage.security;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.Date;
 
 import static java.lang.Thread.sleep;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -12,53 +18,53 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 @Slf4j
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest
 public class JwtTest {
 
-    private Jwt jwt;
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
 
-    @BeforeAll
-    void setUp() {
-        String issuer = "doongjipage";
-        String clientSecret = "366e66f3-166b-4c0d-8502-f35cf9b85d6a";
-        int expirySeconds = 10;
-
-        jwt = new Jwt(issuer, clientSecret, expirySeconds);
-    }
+    @Value("${jwt.token.expirationTime}")
+    private long expirySeconds;
 
     @Test
     void JWT_생성_후_복호화() {
-        Jwt.Claims claims = Jwt.Claims.of(1L, "doongji.team@gmail.com", new String[]{"ROLE_USER"});
-        String encodedJWT = jwt.newToken(claims);
+        Long accountId = 1L;
+        String email = "doongji.team@gmail.com";
+        String[] roles = new String[]{"ROLE_USER"};
+        String encodedJWT = jwtTokenUtil.createToken(accountId, email, roles);
         log.info("encodedJWT: {}", encodedJWT);
 
-        Jwt.Claims decodedJWT = jwt.verify(encodedJWT);
+        jwtTokenClaims decodedJWT = new jwtTokenClaims(jwtTokenUtil, encodedJWT);
         log.info("decodedJWT: {}", decodedJWT);
 
-        assertThat(claims.accountId, is(decodedJWT.accountId));
-        assertThat(claims.email, is(decodedJWT.email));
-        assertArrayEquals(claims.roles, decodedJWT.roles);
+        assertThat(accountId, is(decodedJWT.getAccountId()));
+        assertThat(email, is(decodedJWT.getEmail()));
+        assertArrayEquals(roles, decodedJWT.getRoles());
     }
 
     @Test
     void JWT_갱신() throws Exception {
-        if (jwt.getExpirationTime() > 0) {
-            Jwt.Claims claims = Jwt.Claims.of(1L, "doongji.team@gmail.com", new String[]{"ROLE_USER"});
-            String encodedJWT = jwt.newToken(claims);
+        if(expirySeconds > 0 ) {
+            Long accountId = 1L;
+            String email = "doongji.team@gmail.com";
+            String[] roles = new String[]{"ROLE_USER"};
+            String encodedJWT = jwtTokenUtil.createToken(accountId, email, roles);
             log.info("encodedJWT: {}", encodedJWT);
 
             // 1초 대기 후 토큰 갱신
             sleep(1_000L);
 
-            String encodedRefreshedJWT = jwt.refreshToken(encodedJWT);
+            String encodedRefreshedJWT = jwtTokenUtil.refreshToken(accountId, email, roles, new Date());
             log.info("encodedRefreshedJWT: {}", encodedRefreshedJWT);
 
             assertThat(encodedJWT, not(encodedRefreshedJWT));
 
-            Jwt.Claims oldJwt = jwt.verify(encodedJWT);
-            Jwt.Claims newJwt = jwt.verify(encodedRefreshedJWT);
+            jwtTokenClaims oldJwt = new jwtTokenClaims(jwtTokenUtil, encodedJWT);
+            jwtTokenClaims newJwt = new jwtTokenClaims(jwtTokenUtil, encodedRefreshedJWT);
 
-            long oldExp = oldJwt.exp();
-            long newExp = newJwt.exp();
+            long oldExp = oldJwt.getExp().getTime();
+            long newExp = newJwt.getExp().getTime();
 
             // 1초 대기 후 토큰 갱신했기 때문에 만료시간 차이는 1초 이상
             assertThat(newExp >= oldExp + 1_000L, is(true));
