83abbb11195d0304f9fcceae8376f6bbb0704e0f
Hyojin Song <hyojin1076@naver.com>
2021-09-26 00:11:04 +0900

    feat: 게시판 리스트 조회 및 상세조회 Controller

---
+++ server/src/main/java/com/doongji/homepage/controller/v1/board/BoardController.java
@@ -0,0 +1,43 @@
+package com.doongji.homepage.controller.v1.board;
+
+import com.doongji.homepage.controller.v1.board.dto.BoardResponse;
+import com.doongji.homepage.service.BoardService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Api(tags = "게시판 APIs")
+@RequestMapping("api/v1")
+@RequiredArgsConstructor
+@Controller
+public class BoardController {
+
+    private final BoardService boardService;
+
+    @ApiOperation(value = "게시판 목록")
+    @GetMapping("board")
+    public ResponseEntity<List<BoardResponse>> list() {
+        return ResponseEntity.ok(
+                boardService.list().stream()
+                        .map(BoardResponse::new)
+                        .collect(Collectors.toList())
+        );
+    }
+
+    @ApiOperation(value = "게시판 상세 조회")
+    @GetMapping("board/{boardId}")
+    public ResponseEntity<BoardResponse> board(@PathVariable Long boardId) {
+        return ResponseEntity.ok(
+                new BoardResponse(boardService.findById(boardId))
+        );
+    }
+
+}
---
+++ server/src/main/java/com/doongji/homepage/controller/v1/board/dto/BoardResponse.java
@@ -0,0 +1,37 @@
+package com.doongji.homepage.controller.v1.board.dto;
+
+import com.doongji.homepage.entity.board.AccessType;
+import com.doongji.homepage.entity.board.Board;
+import com.doongji.homepage.entity.board.BoardType;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import static org.springframework.beans.BeanUtils.copyProperties;
+
+@Getter
+@Setter
+@ToString
+public class BoardResponse {
+
+    @ApiModelProperty(value = "PK", required = true)
+    public Long boardId;
+
+    @ApiModelProperty(value = "게시판 이름", required = true)
+    public String title;
+
+    @ApiModelProperty(value = "게시판 설명", required = true)
+    public String description;
+
+    @ApiModelProperty(value = "게시판 유형", required = true)
+    public BoardType boardType;
+
+    @ApiModelProperty(value = "공개 여부", required = true)
+    public AccessType accessScope;
+
+    public BoardResponse(Board source) {
+        copyProperties(source, this);
+    }
+
+}
