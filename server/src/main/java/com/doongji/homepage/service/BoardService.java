a40e9e734836c069ef903517d842750fbe2af4f2
Hyojin Song <hyojin1076@naver.com>
2021-09-26 00:01:33 +0900

    feat: 게시판 목록 출력

---
+++ server/src/main/java/com/doongji/homepage/service/BoardService.java
@@ -0,0 +1,22 @@
+package com.doongji.homepage.service;
+
+import com.doongji.homepage.entity.board.Board;
+import com.doongji.homepage.repository.BoardRepository;
+import com.doongji.homepage.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class BoardService {
+
+    private final BoardRepository boardRepository;
+    private final PostRepository postRepository;
+
+    public List<Board> list() {
+        return boardRepository.findAll();
+    }
+
+}
