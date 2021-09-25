fef6d9ec36dcda5f9f2bece8d7c31d2fa3efcfd2
Hyojin Song <hyojin1076@naver.com>
2021-09-26 01:50:45 +0900

    refactor: 게시판 엔터티 수정

--- server/src/main/java/com/doongji/homepage/entity/board/Board.java
+++ server/src/main/java/com/doongji/homepage/entity/board/Board.java
@@ -1,10 +1,7 @@
 package com.doongji.homepage.entity.board;
 
 import com.doongji.homepage.entity.BaseTimeEntity;
-import lombok.AccessLevel;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 
@@ -12,6 +9,7 @@ import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 import static org.apache.commons.lang3.StringUtils.isNotEmpty;
 
+@ToString
 @Getter
 @Entity
 @EqualsAndHashCode(of = "boardId", callSuper = false)
@@ -34,23 +32,24 @@ public class Board extends BaseTimeEntity {
     @Enumerated(EnumType.STRING)
     private AccessType accessType;
 
-    public Board(String title, String description, BoardType boardType, AccessType accessType) {
+    @Builder
+    public Board(Long boardId, String title, String description, BoardType boardType, AccessType accessType) {
         checkArgument(isNotEmpty(title), "title must be provided.");
         checkArgument(isNotEmpty(description), "description must be provided.");
         checkNotNull(boardType, "boardType must be provided.");
-        checkNotNull(accessType, "accessScope must be provided.");
+        checkNotNull(accessType, "accessType must be provided.");
 
+        this.boardId = boardId;
         this.title = title;
         this.description = description;
         this.boardType = boardType;
         this.accessType = accessType;
     }
 
-    public void updateBoard(Board board) {
-        this.title = board.getTitle();
-        this.description = board.getDescription();
-        this.boardType = board.getBoardType();
-        this.accessType = board.getAccessType();
+    public void updateBoard(String title, String description, BoardType boardType) {
+        this.title = title;
+        this.description = description;
+        this.boardType = boardType;
     }
 
     public void updateAccessType(AccessType accessType) {
