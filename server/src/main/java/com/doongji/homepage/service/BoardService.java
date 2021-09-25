afaa5df71f4f04e1ec231bf03560192bc6e83fff
Hyojin Song <hyojin1076@naver.com>
2021-09-26 00:02:48 +0900

    feat: 게시판 등록 및 수정

--- server/src/main/java/com/doongji/homepage/service/BoardService.java
+++ server/src/main/java/com/doongji/homepage/service/BoardService.java
@@ -1,12 +1,15 @@
 package com.doongji.homepage.service;
 
+import com.doongji.homepage.entity.board.AccessType;
 import com.doongji.homepage.entity.board.Board;
+import com.doongji.homepage.entity.board.BoardType;
 import com.doongji.homepage.exception.NotFoundException;
 import com.doongji.homepage.repository.BoardRepository;
 import com.doongji.homepage.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -24,5 +27,23 @@ public class BoardService {
         return boardRepository.findById(boardId)
                 .orElseThrow(() -> new NotFoundException(Board.class, boardId));
     }
+
+    @Transactional
+    public Board register(String title, String description, BoardType boardType) {
+        return boardRepository.save(
+                new Board(title, description, boardType, AccessType.PUBLIC)
+        );
+    }
+
+    @Transactional
+    public Board update(Long id, String title, String description, BoardType boardType) {
+        return boardRepository.findById(id)
+                .map(board ->{
+                    board.updateBoard(board);
+                    return board;
+                })
+                .orElseThrow(() -> new NotFoundException(Board.class, id));
+    }
+
 
 }
